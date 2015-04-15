f735d8071eaba70835872ac362de2ac65bc5bd56
nikunjM <nikunj.mange@mavs.uta.edu>
2015-04-14 18:28:49 -0600

    Update last_twitter_status.rb
    added favorite tweet, and you can search any phrases.

--- last_twitter_status.rb
+++ last_twitter_status.rb
@@ -11,23 +11,23 @@ client = Twitter::REST::Client.new do |config|
 end
 
 #add authentication for different users.
-print "please Enter the key word you want to follow?"
+print "please Enter the key word or phares you want to search?"
 keyword_value= gets.chomp;
-print "Enter the tweets or follows you want to find "
-print "for ex number from 1- 100";
+print "Number of followers you want to follow(Please enter number from 1 to 100)"
 count_number = gets.chomp;
 count=1;
 #validate user inputs, using reg exp.
 
 client.search(keyword_value, result_type: "recent").take(Integer(count_number)).collect do |tweet|
-  puts "Count       Username  				tweet"           
-  puts "(#{tweet.url})    #{tweet.user.screen_name} #{tweet.text}";
+  puts "Count              Tweet URl                 Username                                    tweet"           
+  puts "(#{count})      #{tweet.url})    #{tweet.user.screen_name}                          #{tweet.text}";
   sleep(3);
   count=count+1;
-  #before following user check if its alreay in list using bollean.	
-  client.follow("#{tweet.user.screen_name}")
+  client.follow("#{tweet.user.screen_name}");
   #client.favorite.create("#{tweet.user.screen_name}")
-   client.favorites("#{tweet.id}","#{tweet.user.screen_name}",tweet.url)
+  #client.favorites("#{tweet.url}");
+  client.fav tweet;  
+#client.favorites("#{tweet.id}","#{tweet.user.screen_name}",https://api.twitter.com/1.1/favorites/create.json)
 end
 
 
@@ -35,6 +35,3 @@ end
 #the above link is for oAuth and can be used for signing of new user.
 #Can request a json file and also post a tweet.
 #https://dev.twitter.com/rest/reference/post/favorites/create
-
-
-
